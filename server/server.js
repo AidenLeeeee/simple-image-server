7418c1f086c1ab8fd6aed326e81e6b7950337523
AidenLeeeee <hoo8799@gmail.com>
2023-05-27 13:34:16 +0900

    connect to mongodb

--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
+require("dotenv").config();
 const express = require("express");
 const multer = require("multer");
 const { v4: uuid } = require("uuid");
 const mime = require("mime-types");
+const mongoose = require("mongoose");
 
 const storage = multer.diskStorage({
     destination: (req, file, cb) => cb(null, "./uploads"),
@@ -24,14 +26,19 @@ const upload = multer({
 const app = express();
 const PORT = 8799;
 
-app.use("/uploads", express.static("uploads"));
-
-app.post("/upload", upload.single("image"), (req, res) => {
-    console.log(req.file);
-
-    res.json(req.file);
-});
-
-app.listen(PORT, () =>
-    console.log("Express server listening on PORT " + PORT + "...")
-);
+mongoose
+    .connect(process.env.MONGO_URI, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => {
+        console.log("mongodb connected.");
+        app.use("/uploads", express.static("uploads"));
+        app.post("/upload", upload.single("image"), (req, res) =>
+            res.json(req.file)
+        );
+        app.listen(PORT, () =>
+            console.log("Express server listening on PORT " + PORT + "...")
+        );
+    })
+    .catch((err) => console.log(err));
